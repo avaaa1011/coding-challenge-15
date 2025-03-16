5983d240add7b5b108f1d85f8949d5ca26f00987
Ava Tran <ava.tran1011@gmail.com>
2025-03-15 23:53:35 -0400

    task 1: base structure created

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+//task 1: creating base structure
+document.addEventListener("DOMContentLoaded", () => 
+{
+    console.log("Risk Dashboard Loaded"); 
+    const riskDashboard = document.getElementById("riskDashboard"); //selects container in the html
+});
+   
