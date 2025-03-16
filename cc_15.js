929bb33eac42c45ae389f834b54249a913200ea1
Ava Tran <ava.tran1011@gmail.com>
2025-03-16 00:39:45 -0400

    tak 4: risk categorization

--- cc_15.js
+++ cc_15.js
@@ -1,7 +1,17 @@
 //task 1: creating base structure
-document.addEventListener("DOMContentLoaded", () => 
+document.addEventListener("DOMContentLoaded", () =>
 {
     console.log("Risk Dashboard Loaded"); 
+    const form = document.getElementById("inputForm"); 
+
+    form.addEventListener("submit", function(event) 
+    { 
+        const riskName = document.getElementById("risk").value 
+        const riskLevel = document.getElementById("riskLevel").value 
+        const department = document.getElementById("department").value 
+        addRiskItem(riskName, riskLevel, department); 
+        this.reset(); 
+    });
 });
 
 //task 2: adding risk items dynamically
@@ -11,6 +21,16 @@ function addRiskItem(riskName, riskLevel, department) //function that creates ne
     const riskCard = document.createElement("div"); //adding new card with div
     riskCard.classList.add("riskCard"); 
 
+    //task 4: categorizing risks by their level through different colors
+    if (riskLevel === "Low") {
+        riskCard.classList.add("lowRisk"); 
+    } else if (riskLevel === "Medium") {
+        riskCard.classList.add("mediumRisk"); 
+    } else if (riskLevel === "High") {
+        riskCard.classList.add("highRisk"); 
+    }
+    riskCategories();
+
     //each risk card will contain: 
     const name = document.createElement("h3"); 
         name.textContent = `Risk Name: ${riskName}`;
@@ -19,7 +39,6 @@ function addRiskItem(riskName, riskLevel, department) //function that creates ne
 
     const department = document.createElement("p"); 
         department.textContent = `Department: ${department}`;
-
 
     //task 3: removing risk items
     //adding a resolve button to modify addRiskItems
@@ -35,9 +54,17 @@ function addRiskItem(riskName, riskLevel, department) //function that creates ne
     riskCard.appendChild(resolveButton);
     riskDashboard.appendChild(riskCard); 
 }
+//task 4 : categorizing risk by levels
+function riskCategories() //assigning each risk level a color based on if they are high, medium, or low
+{
+    document.querySelectorAll(".highRisk").forEach(item => {item.style.backgroundColor = "rgb(244, 74, 74)"});
+    document.querySelectorAll(".mediumRisk").forEach(item => { item.style.backgroundColor = "rgb(252, 255, 159)"});
+    document.querySelectorAll(".lowRisk").forEach(item => { item.style.backgroundColor = "rgb(182, 254, 136)"});
+}
 // Test Cases
 addRiskItem("Data Breach", "High", "IT");
 addRiskItem("Supply Chain Disruption", "Medium", "Operations");
 addRiskItem("Market Fluctuations", "High", "Finance"); // Clicking "Resolve" should remove this risk from the dashboard.
-
+addRiskItem("Cybersecurity Threat", "High", "IT");
+addRiskItem("HR Compliance Issue", "Low", "Human Resources");
    
